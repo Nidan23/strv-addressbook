38252fe0d9a9ce535d424f8ec94578d5154df508
Nidan23 <dev.sulkowski@gmail.com>
2022-06-11 00:16:15 +0200

    [#4][ADD] Added register & login functions -> improvement is needed

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,15 +2,14 @@ import * as express from 'express'
 import {DataSource, Repository} from "typeorm";
 import {UserEntity} from "../entity/user.entity";
 import {User} from "../interface/user.type";
+import {userDataSource} from "../service/database.service";
 
 export default class AuthController {
     public path = '/auth'
     public router = express.Router()
-    private readonly userRepository: any
 
-    constructor(private userDataSource: DataSource) {
-        // this.userDataSource.manager.getRepository<UserEntity>(UserEntity)
-        this.userRepository = this.userDataSource.manager
+    constructor() {
+        this.initDatabaseConnection()
         this.initRoutes()
     }
 
@@ -19,23 +18,42 @@ export default class AuthController {
         this.router.post(`${this.path}/register`, this.register)
     }
 
+    public initDatabaseConnection(){
+        userDataSource
+            .initialize()
+            .then(() => {
+                console.log("Data Source has been initialized!")
+            })
+            .catch((err) => {
+                console.error("Error during Data Source initialization:", err)
+            })
+    }
+
     private login(request: express.Request, response: express.Response){
-        const user: User = { email: "loluwa", password: "passs"}
-        this.userRepository?.save(user)
+        const user: User = request.body
+
+        userDataSource.getRepository(UserEntity).findOne({ where: user})
             .then(r => {
-                console.log(r)
-                this.userRepository.find()
-                    .then(r => {
-                        console.log(r)
-                        response.send(r.toString())
-                    })
+                response.send("Logged Successfully")
             })
             .catch(err => {
                 console.log(err)
+
+                response.status(500).send("Something went wrong. Your login data isn't correct")
             })
     }
 
     private register(request: express.Request, response: express.Response){
-        response.redirect('/auth/login')
+        const user: User = request.body
+
+        userDataSource.getRepository(UserEntity).save(user)
+            .then(r => {
+                response.redirect('/auth/login')
+            })
+            .catch(err => {
+                console.log(err)
+
+                response.status(500).send("Something went wrong. Probably email is already taken")
+            })
     }
 }
