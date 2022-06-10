e8df771c23a7c65843ccc00df91ddc9a392b3c3d
Nidan23 <dev.sulkowski@gmail.com>
2022-06-11 01:07:03 +0200

    [#4][ADD] Added basic authService

---
+++ src/service/auth.service.ts
@@ -0,0 +1,12 @@
+import {User} from "../interface/user.type";
+
+export class AuthService{
+
+    public static getJwtToken(user: User){
+
+    }
+
+    public static resolveDataFromJwtToken(user: User){
+
+    }
+}
