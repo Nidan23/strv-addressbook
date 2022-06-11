a4e8c629c4637d52a6acf83cc7c2b1afc30b09ca
Nidan23 <dev.sulkowski@gmail.com>
2022-06-11 20:49:52 +0200

    [#5][ADD] Added firebase connection

--- src/service/firebase.service.ts
+++ src/service/firebase.service.ts
@@ -1,3 +1,33 @@
+import {initializeApp, cert} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import {User} from "../interface/user.type";
+import {Contact} from "../interface/contact.type";
+import {firestore} from "firebase-admin";
+import Firestore = firestore.Firestore;
+import {VariableService} from "./variable.service";
+
 export class FirebaseService{
 
+    private static db: Firestore
+    private static app
+
+    public static firebaseInit(){
+        this.app = initializeApp({
+            credential: cert(VariableService.certPath)
+        });
+
+        this.db = getFirestore(this.app)
+    }
+
+    public static async addContact(user: User, payload: Contact): Promise<boolean>{
+        const userContactsPath = this.db.collection(VariableService.userCollectionPath).doc(user.email).collection(VariableService.contactsCollectionPath)
+        const contactPath = userContactsPath.doc(payload.lastName)
+
+        const returnValue = await contactPath.set(payload)
+            .catch(err => {
+                console.log(err)
+            })
+
+        return !!returnValue;
+    }
 }
