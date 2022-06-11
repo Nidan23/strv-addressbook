18a025c24091ae8ddeb9260d5d001cb5f08e73d4
Nidan23 <dev.sulkowski@gmail.com>
2022-06-11 20:50:13 +0200

    [#5][ADD] Added contact add logic

--- src/controller/contacts.controller.ts
+++ src/controller/contacts.controller.ts
@@ -1,26 +1,37 @@
 import * as express from 'express'
 import {AuthService} from "../service/auth.service";
+import {UserValidation} from "../interface/validation.type";
+import {Contact} from "../interface/contact.type";
+import {FirebaseService} from "../service/firebase.service";
 
 export default class ContactsController {
     public path = '/contacts'
     public router = express.Router()
+    private app: any
+    private analytics: any
 
     constructor() {
         this.initRoutes()
+        this.initFirebase()
     }
 
     public initRoutes(){
         this.router.post(`${this.path}/addContacts`, this.addContacts)
-        this.router.get(`${this.path}/getContacts`, this.getContacts)
+    }
+    public initFirebase(){
+        FirebaseService.firebaseInit()
     }
 
     private async addContacts(request: express.Request, response: express.Response){
-        if((await AuthService.validateJwtToken(request.body.token)))
-            return response.send("Token validated")
-        return response.send('Not valid')
-    }
+        const contact: Contact = request.body.data
+        const validationData: UserValidation = await AuthService.validateJwtToken(request.body.token)
 
-    private getContacts(request: express.Request, response: express.Response){
-        response.send('Your contacts bro')
+        if(!validationData.isValid)
+            return response.send('Not valid')
+
+
+        if(await FirebaseService.addContact(validationData.data, contact))
+            return response.send("Contact added 2 your account")
+
     }
 }
