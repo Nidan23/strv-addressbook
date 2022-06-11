0062cd8c9ab6a0a59456392cefa778c00c6da1bb
Nidan23 <dev.sulkowski@gmail.com>
2022-06-11 17:40:49 +0200

    [#3][ADD] Added jwt generate & verify methods

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,20 +1,37 @@
 import {User} from "../interface/user.type";
+import * as jwt from "jsonwebtoken"
+import {VariableService} from "./variable.service";
+import {UserService} from "./user.service";
+import {AuthorizeUserType} from "../interface/authorizeUser.type";
 
 export class AuthService{
 
-    public static getJwtToken(){
+    public static getJwtToken(user: User){
+        return this.generateJwtToken(user)
+    }
+
+    public static resolveDataFromJwtToken(token: string){
+        return jwt.verify(token, VariableService.secretKey)
+    }
+
+    public static async validateJwtToken(token: string): Promise<boolean>{
+        try {
+            const {email, password} = this.resolveDataFromJwtToken(token) as AuthorizeUserType
+            const user: User = {email, password} as User
+
+            return (await UserService.findUser(user));
+        } catch (e){
+            console.log(VariableService.verifyTokenError)
+
+            return false
+        }
 
     }
 
-    public static resolveDataFromJwtToken(user: User){
-
-    }
-
-    public static validateJwtToken(token: string){
-
-    }
-
-    public static generateJwtToken(user: User){
-
+    private static generateJwtToken(user: User){
+        const {email, password} = user
+        return jwt.sign({email, password}, VariableService.secretKey, {
+            expiresIn: VariableService.tokenExpirationTime
+        })
     }
 }
